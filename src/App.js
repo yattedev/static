a6bf1c1781a6daba615c75122da965b2935174d5
wada <kawanishi9726@gmail.com>
2018-11-02 01:08:23 +0900

    wip

--- src/App.js
+++ src/App.js
@@ -9,28 +9,55 @@ import Playlog from './pages/Playlog'
 import Login from './pages/Login'
 import Signup from './pages/Signup'
 
-const App = () => (
-  <BrowserRouter>
-    <div>
-      <ul>
-        <li><Link to='/'>ホーム</Link></li>
-        <li><Link to='/user'>ユーザー</Link></li>
-        <li><Link to='/scenario'>シナリオ</Link></li>
-        <li><Link to='/playlog'>プレイログ</Link></li>
-        <li><Link to='/login'>ログイン</Link></li>
-        <li><Link to='/signup'>サインアップ</Link></li>
-      </ul>
+class App extends React.Component{
+  constructor(){
+    super();
+    this.state = {
+      login:false,
+      data:{}
+    }
+  }
 
-      <div>
-        <Route exact path='/' component={Home}/>
-        <Route path='/user' component={User} />
-        <Route path='/scenario' component={Scenario} />
-        <Route path='/playlog' component={Playlog} />
-        <Route path='/login' component={Login} />
-        <Route path='/signup' component={Signup} />
-      </div>
-    </div>
-  </BrowserRouter>
-)
+  async componentDidMount () {
+    const json = JSON.parse(localStorage.getItem('loginData'))
+    if (json != {}){
+      this.setState({
+        login:true,
+        data:json
+      })
+    }
+  }
+  
+  login() {
+    this.setState({ login: true });
+  }
+
+
+  render(){
+    return(
+      <BrowserRouter>
+        <div>
+          <ul>
+            <li><Link to='/'>ホーム</Link></li>
+            <li><Link to='/user'>ユーザー</Link></li>
+            <li><Link to='/scenario'>シナリオ</Link></li>
+            <li><Link to='/playlog'>プレイログ</Link></li>
+            { !(this.state.login) && <li><Link to='/login'>ログイン</Link></li>}
+            { !(this.state.login) && <li><Link to='/signup'>サインアップ</Link></li>}
+          </ul>
+
+          <div>
+            <Route exact path='/' component={Home}/>
+            <Route path='/user' component={User} />
+            <Route path='/scenario' component={Scenario} />
+            <Route path='/playlog' component={Playlog} />
+            <Route path='/login' component={Login} />
+            <Route path='/signup' component={Signup} />
+          </div>
+        </div>
+      </BrowserRouter>
+    )
+  }
+}
 
 export default App;
